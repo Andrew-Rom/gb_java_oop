638cb316e73c9b44e7032ab8922f235ab96eef39
Andrew-Rom <1811mail@gmail.com>
2023-03-22 22:30:23 +0300

    Changed creating teams method. Changed makeStep().

--- src/Init.java
+++ src/Init.java
@@ -11,36 +11,20 @@ public class Init {
         Main.teamWhite = new ArrayList<>();
         for (int i = 1; i < Main.GANG_SIZE + 1; i++) {
             switch (new Random().nextInt(4)) {
-                case 0 -> {
-                    Main.teamWhite.add(new Monk(getName(), 1, i));
-                }
-                case 1 -> {
-                    Main.teamWhite.add(new XBowMan(getName(), 1, i));
-                }
-                case 2 -> {
-                    Main.teamWhite.add(new Spearman(getName(), 1, i));
-                }
-                default -> {
-                    Main.teamWhite.add(new Peasant(getName(), 1, i));
-                }
+                case 0 -> Main.teamWhite.add(new Monk(getName(), 1, i));
+                case 1 -> Main.teamWhite.add(new XBowMan(getName(), 1, i));
+                case 2 -> Main.teamWhite.add(new Spearman(getName(), 1, i));
+                default -> Main.teamWhite.add(new Peasant(getName(), 1, i));
             }
         }
 
         Main.teamBlack = new ArrayList<>();
         for (int i = 1; i < Main.GANG_SIZE + 1; i++) {
             switch (new Random().nextInt(4)) {
-                case 0 -> {
-                    Main.teamBlack.add(new Witch(getName(), 10, i));
-                }
-                case 1 -> {
-                    Main.teamBlack.add(new Sniper(getName(), 10, i));
-                }
-                case 2 -> {
-                    Main.teamBlack.add(new Bandit(getName(), 10, i));
-                }
-                default -> {
-                    Main.teamBlack.add(new Peasant(getName(), 10, i));
-                }
+                case 0 -> Main.teamBlack.add(new Witch(getName(), 10, i));
+                case 1 -> Main.teamBlack.add(new Sniper(getName(), 10, i));
+                case 2 -> Main.teamBlack.add(new Bandit(getName(), 10, i));
+                default -> Main.teamBlack.add(new Peasant(getName(), 10, i));
             }
         }
 
